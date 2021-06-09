d63f4da0d3cf945bf7a19acf466a3626e60a8c37
sadhu <1714222385@qq.com>
2021-06-09 17:51:21 +0800

    feat: 维持登录状态

--- src/components/project-list/index.tsx
+++ src/components/project-list/index.tsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { SearchPanel } from './search-panel';
 import { List } from './list';
 import { cleanObject, useDebouncedState, useMount } from '../../utils';
-import * as qs from 'qs';
-
-const apiUrl = process.env.REACT_APP_API_URL;
+import { useHttp } from '../../utils/http';
 
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
@@ -16,21 +14,18 @@ export const ProjectListScreen = () => {
   const debouncedParam = useDebouncedState(param, 1000);
 
   const [list, setList] = useState([]);
+  const client = useHttp();
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    // TODO catch 呢？
+    client('projects', {
+      data: cleanObject(debouncedParam),
+    }).then(setList)
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    // TODO catch 呢？
+    client("users").then(setUsers);
   });
 
   return (
--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,11 +1,24 @@
 import React, { ProviderProps, ReactNode, useState } from 'react';
 import * as auth from '../auth-provider';
 import { User } from '../components/project-list/search-panel';
+import { http } from '../utils/http';
+import { useMount } from '../utils/index';
 
 interface AuthForm {
   username: string;
   password: string;
 }
+
+// 所谓登录状态维持，就是刷新时跳到首页，在首页判断本地有无 token，有 token 则请求一下数据，跳到登录后的页面，没 token 的话就跳到登录前页面
+const bootstrapUser = async () => {
+  let user = null;
+  const token = auth.getToken();
+  if (token) {
+    const data = await http("me", { token });
+    user = data.user;
+  }
+  return user;
+};
 
 const AuthContext =
   React.createContext<
@@ -26,6 +39,11 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
+
+  useMount(() => {
+    // 登录状态维持
+    bootstrapUser().then(setUser);
+  })
 
   return <AuthContext.Provider value={{ user, login, register, logout }}>{children}</AuthContext.Provider>;
 };
